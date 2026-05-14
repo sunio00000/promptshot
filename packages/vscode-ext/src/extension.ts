848201b259c573ef87a38fbefafeca6ee004f70b
sunio <sunio@pubg.com>
2026-05-14 10:56:44 +0900

    chore: scaffold promptshot VS Code extension package

---
+++ packages/vscode-ext/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode'
+
+export function activate(_context: vscode.ExtensionContext): void {
+  // Commands wired up in later tasks.
+}
+
+export function deactivate(): void {}
